f2ed21e551ca7554c8e9685df038bbcecb083120
Bryan Chua <me@bryanchua.com>
2020-06-02 13:14:42 +0800

    fixing deployment error

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import router from './router';
 import store from './store';
 import Amplify from '@aws-amplify/core';
 //@ts-ignore
-import awsconfig from './aws-exports';
+import awsconfig from '@/aws-exports';
 
 Amplify.configure(awsconfig);
 Vue.config.productionTip = false;
